07dc4a9ec943387bd732622499725410de4a2839
Fazt <fazt@faztweb.com>
2022-08-06 01:22:53 +0200

    proxy añadido

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Hello world</h1>
       <button onClick={async () => {
-        const response = await fetch('http://localhost:3000/users')
+        const response = await fetch('/users')
         const data = await response.json()
         console.log(data)
       }}>Obtener datos</button>
